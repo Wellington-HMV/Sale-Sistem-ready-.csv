5dd6c9256da3ac43c4bf4fe32fea8f76fb4cd11d
Wellington <well.raiser@gmail.com>
2019-06-28 07:41:18 -0300

    Create data in files

---
+++ secao-13-exePropFilesDirectory-class189/Entities/Products.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    class Products
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Products(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+        public double SumSales()
+        {
+            return Price* Quantity;
+        }
+    }
+}
--- secao-13-exePropFilesDirectory-class189/Program.cs
+++ secao-13-exePropFilesDirectory-class189/Program.cs
@@ -2,6 +2,7 @@
 using System.Globalization;
 using System.IO;
 using System.Collections.Generic;
+using Entities;
 
 namespace secao_13_exePropFilesDirectory_class189
 {
@@ -9,7 +10,7 @@ namespace secao_13_exePropFilesDirectory_class189
     {
         static void Main(string[] args)
         {
-            string Path = @"F:\Users\wellv\Documents\Programação Orientada a Objeto C#\secao-13-exePropFilesDirectory-class189\Salles\";
+            string Pathd = @"F:\Users\wellv\Documents\Programação Orientada a Objeto C#\secao-13-exePropFilesDirectory-class189\Salles\";
 
             try
             {
@@ -17,11 +18,11 @@ namespace secao_13_exePropFilesDirectory_class189
 
                 Console.Write("Digit your new file: ");
                 string nFile = Console.ReadLine();
-                string path = Path + nFile;
+                string path = Pathd + nFile;
 
                 char insertSales = 'y';
                 List<string> list = new List<string>();
-                while (insertSales== 'y')
+                while (insertSales == 'y')
                 {
                     Console.Write("insert your sale: ");
                     string sales = Console.ReadLine();
@@ -37,11 +38,31 @@ namespace secao_13_exePropFilesDirectory_class189
                         string name = fields[0];
                         double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
                         int quantity = int.Parse(fields[2]);
+                        Products prod = new Products(name, price, quantity);
+                        sw.WriteLine("Product name:" + prod.Name + ", " + "R$" + prod.Price.ToString("F2", CultureInfo.InstalledUICulture) + ", Quantities: " + prod.Quantity);
+                    }
+                }
+                string sourcetPath = Path.GetDirectoryName(nFile);
+                string targetFolderPath = sourcetPath + @"\out";
+                string targetFilePath = targetFolderPath + $@"\{nFile}_summary.csv";
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (string x in list)
+                    {
+                        string[] fields = x.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+                        Products prod = new Products(name, price, quantity);
+                        sw.WriteLine("Product name:" + prod.Name + ", " + "R$" + prod.SumSales().ToString("F2", CultureInfo.InstalledUICulture));
                     }
                 }
             }
             catch (IOException e)
             {
+                Console.WriteLine("An error occurred!");
                 Console.WriteLine(e.Message);
             }
         }
